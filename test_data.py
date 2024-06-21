384a10fd86eb8a26792f4e597d5cf67b064791cd
i-deal <ianmdeal@gmail.com>
2024-06-20 21:00:34 -0400

    update tests

--- test_data.py
+++ test_data.py
@@ -1,18 +1,20 @@
 # prerequisites
 from dataset_builder import Dataset
-from dataset_builder_old import dataset_builder
+#from dataset_builder_old import dataset_builder
 from torchvision import utils
 import time
+import torch
 
-transform = {'retina':True, 'colorize':True, 'location_targets':{'left':[0,1,2,3], 'right':[4,5,6,7]}, 'color_targets':{4:[4,3,7], 1:[8,1], 2:[5,6]}}
-train_data = Dataset('emnist', transform)
+transform = {'retina':True, 'colorize':True, 'scale':True, 'location_targets':{'left':[0,1,2,3], 'right':[4,5,6,7]}, 'color_targets':{4:[4,3,7], 1:[8,1], 2:[5,6]}, 
+             'scale_targets': {0:[0,1,2,3,4],1:[5,6,7,8,9]}}
+train_data = Dataset('mnist', transform)
 data_ranges = train_data.all_possible_labels()
 #print(data_ranges)
 train_loader = train_data.get_loader(110)
-data,labels = iter(train_loader).next()
+data,labels = next(iter(train_loader))
 
 #print(labels[0][0:100])
-utils.save_image(data[0][0:100],'datasettest.png',1)
+utils.save_image(data[0][0:10],'datasettest.png')
 s = 0
 for i in range(len(labels[0])):
     shape = labels[0][i].item()
