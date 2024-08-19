9948a5366c3fd7430eeb2746ea2169b62789dc27
i-deal <ianmdeal@gmail.com>
2024-08-19 19:32:13 -0400

    update trainlabels

--- train_labels.py
+++ train_labels.py
@@ -3,18 +3,27 @@ import torch
 from mVAE import vae, load_checkpoint
 from torch.utils.data import DataLoader, ConcatDataset
 from dataset_builder import Dataset
-load_checkpoint('output_emnist_recurr/checkpoint_150.pth')
+import os
+v=2
+
+checkpoint_folder_path = f'output_label_net/' # the output folder for the trained model versions
+
+if not os.path.exists(checkpoint_folder_path):
+    os.mkdir(checkpoint_folder_path)
+
+load_checkpoint(f'output_mnist_2drecurr{v}/checkpoint_most_recent.pth')
 bs = 50
 #load_checkpoint_shapelabels('output_label_net/checkpoint_shapelabels5.pth')
 
 transforms = { 'colorize':True}
 
-emnist_dataset = Dataset('emnist', transforms)
+#emnist_dataset = Dataset('emnist', transforms)
 mnist_dataset = Dataset('mnist', transforms)
-train_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_dataset, mnist_dataset, mnist_dataset]), batch_size=bs, shuffle=True,  drop_last= True)
+mnist_loader = mnist_dataset.get_loader(bs)
+#train_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_dataset, mnist_dataset, mnist_dataset]), batch_size=bs, shuffle=True,  drop_last= True)
 for epoch in range (1,21):
    
-    train_labels(epoch, train_loader_noSkip)
+    train_labels(epoch, mnist_loader)
         
     if epoch in [1,5,10,20]:
         checkpoint =  {
